723e3001be5347db4b0859a92e16555f3561240e
Ben Christel <bchristel@pivotal.io>
2016-06-05 17:35:51 -0700

    Remove git author initials from bash prompt
    since git pair will no longer be installed

--- scripts/configuration-bash.sh
+++ scripts/configuration-bash.sh
@@ -8,7 +8,6 @@ rm -rf ~/.bash_it
 pushd ~/
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
 popd
-cp files/add_user_initials_to_git_prompt_info.bash ~/.bash_it/custom
 cp files/bobby_pivotal/bobby_pivotal.theme.bash ~/.bash_it/themes/bobby/bobby.theme.bash
 ~/.bash_it/install.sh
 source ~/.bash_profile
